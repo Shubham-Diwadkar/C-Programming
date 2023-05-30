84a7e9fe835484c0e18e58bad762fae2a3ca492b
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2023-05-30 12:43:55 +0530

    Update TAKINGUSERINPUTS.C

--- TAKINGUSERINPUTS.C
+++ TAKINGUSERINPUTS.C
@@ -1,16 +1,25 @@
 //Write a program to take input from a user.
 
+// Including header files
 #include<conio.h>
 #include<stdio.h>
+
+// main function starts
 void main()
 {
-int rollno;
-float per;
-char grade;
-char name[20];
-clrscr();
-printf("\n Enter Values for Roll No., Name, Grade and Percentage of a Student");
-scanf("%d %s %c %f",&rollno,&name,&grade,&per);
-printf("\n Name = %s \n Roll No. = %d \n Percentage = %f \n Grade = %c",name,rollno,per,grade);
-getch();
+  // Variable Declaration
+  int rollno;
+  float per;
+  char grade;
+  char name[20];
+  
+  //Clearing the oputput screen
+  clrscr();
+  
+  printf("\n Enter Values for Roll No., Name, Grade and Percentage of a Student");                  // Printing the statement
+  scanf("%d %s %c %f",&rollno,&name,&grade,&per);                                                   // Fetching the values
+  printf("\n Name = %s \n Roll No. = %d \n Percentage = %f \n Grade = %c",name,rollno,per,grade);   //Printing the fetched values
+  
+  // Waiting till the user presses any key to exit the output screen
+  getch();
 }
